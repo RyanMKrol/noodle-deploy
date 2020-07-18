7f75fcac379dd02620485e3514e5f29f0c27422b
Ryan Krol <ryankr@hotmail.co.uk>
2020-07-18 16:55:37 +0100

    Add yargs integration for fetching CLI args

--- src/index.js
+++ src/index.js
@@ -3,15 +3,27 @@
 import fetchDynamoCredentials from './modules/credentials';
 import readProjectData from './modules/storage';
 
-async function main() {
-  const decryptionPassword = process.argv[2];
-  const projectName = process.argv[3];
+const { argv } = require('yargs')
+  .usage('Usage: $0 [options]')
+  .example('$0 -s <decryption_secret> -p <project_name>')
+  .alias('s', 'secret')
+  .nargs('s', 1)
+  .describe('s', 'Specify the client secret')
+  .alias('p', 'projectName')
+  .nargs('p', 1)
+  .describe('p', 'Specify the project name')
+  .demandOption(['s', 'p'])
+  .help('h')
+  .alias('h', 'help');
 
-  const dynamoCredentials = await fetchDynamoCredentials(decryptionPassword);
+async function main() {
+  const { secret } = argv;
+  const { projectName } = argv;
+
+  const dynamoCredentials = await fetchDynamoCredentials(secret);
   const projectData = await readProjectData(dynamoCredentials, projectName);
 
-  console.log(projectData);
-  console.log(JSON.stringify(projectData));
+  process.stdout.write(JSON.stringify(projectData));
 }
 
 main();
