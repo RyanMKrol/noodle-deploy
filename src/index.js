a5e3407308be2766dbffe8a9f4dc156606da1a56
Ryan Krol <ryankr@hotmail.co.uk>
2020-07-18 15:34:13 +0100

    Add method to decrypt credentials folder and print output

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 #!/usr/bin/env node
-process.stdout.write('Hello World!');
+
+import shell from 'shelljs';
+import util from 'util';
+import path from 'path';
+import fs from 'fs';
+
+const credentialsFolder = `${path.resolve()}/credentials/`;
+
+shell.exec(
+  `openssl aes-256-cbc -d -a -in ${credentialsFolder}/dynamo.json.enc -out ${credentialsFolder}/dynamo.json -k ${process.argv[2]}`,
+);
+
+const readFile = util.promisify(fs.readFile);
+
+readFile(`${credentialsFolder}/dynamo.json`, 'utf8').then((data) => {
+  console.log(JSON.parse(data));
+});
 
 console.log(process.argv);
