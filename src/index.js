8b4e2440d73a4fa05d3a650b4d8f76f7e28ae7c5
Ryan Krol <ryankr@hotmail.co.uk>
2020-07-18 16:23:44 +0100

    Integrate main app with dynamo call to fetch project data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 #!/usr/bin/env node
 
 import fetchDynamoCredentials from './modules/credentials';
+import readProjectData from './modules/storage';
 
-fetchDynamoCredentials(process.argv[2]).then((data) => {
-  console.log(data);
-});
+async function main() {
+  const decryptionPassword = process.argv[2];
+  const projectName = process.argv[3];
+
+  const dynamoCredentials = await fetchDynamoCredentials(decryptionPassword);
+  const projectData = await readProjectData(dynamoCredentials, projectName);
+
+  console.log(projectData);
+  console.log(JSON.stringify(projectData));
+}
+
+main();
