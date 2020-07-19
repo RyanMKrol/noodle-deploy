99bbc4ae374e3b0d79463a2ea52bfafe21cd6ffb
Ryan Krol <ryankr@hotmail.co.uk>
2020-07-19 15:39:27 +0100

    Add CouldNotDecryptCredentials error type

--- src/modules/types/index.js
+++ src/modules/types/index.js
@@ -1,2 +1,4 @@
 export { default as ProjectData } from './ProjectData';
 export { default as CouldNotReadDynamo } from './CouldNotReadDynamo';
+export { default as CouldNotDecryptCredentials } from './CouldNotDecryptCredentials';
+export { default as CouldNotChmodFile } from './CouldNotChmodFile';
