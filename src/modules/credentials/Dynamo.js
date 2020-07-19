6f6ddee5d8bfdd686ce72c435744bca5d693f578
Ryan Krol <ryankr@hotmail.co.uk>
2020-07-19 15:41:27 +0100

    Use CouldNotDecryptCredentials in dynamo credentials file

--- src/modules/credentials/Dynamo.js
+++ src/modules/credentials/Dynamo.js
@@ -2,14 +2,20 @@ import shell from 'shelljs';
 import util from 'util';
 import fs from 'fs';
 
+import { CouldNotDecryptCredentials } from '../types';
+
 const CREDENTIALS_FOLDER = `${__dirname}/../../../credentials`;
 const readFile = util.promisify(fs.readFile);
 
 async function fetchDynamoCredentials(secret) {
   // decrypt the credentials file
-  shell.exec(
+  const commandStatus = shell.exec(
     `openssl aes-256-cbc -d -a -in ${CREDENTIALS_FOLDER}/dynamo.json.enc -out ${CREDENTIALS_FOLDER}/dynamo.json -k ${secret}`,
   );
+
+  if (!commandStatus || commandStatus.code !== 0) {
+    throw new CouldNotDecryptCredentials();
+  }
 
   const dynamoCredentials = JSON.parse(await readFile(`${CREDENTIALS_FOLDER}/dynamo.json`, 'utf8'));
 
