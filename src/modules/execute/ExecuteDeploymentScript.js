c1b37f981e8b8b8dc00f438c468470456f36c4a2
Ryan Krol <ryankr@hotmail.co.uk>
2020-07-19 16:13:04 +0100

    Remove logging of execution script

--- src/modules/execute/ExecuteDeploymentScript.js
+++ src/modules/execute/ExecuteDeploymentScript.js
@@ -9,9 +9,7 @@ export default async function executeDeploymentScript(secret, projectData) {
   // for now I'm only deploying to a single server
   const deploymentServer = deploymentServers[0];
 
-  const deployCommandStatus = shell.exec(
+  shell.exec(
     `ssh -o "StrictHostKeyChecking no" -i ${getAwsCredentialsPath()}  ec2-user@${deploymentServer} "bash -s" < ${DEPLOYMENT_SCRIPT_LOCATION} $AES_PASS`,
   );
-
-  console.log(deployCommandStatus);
 }
